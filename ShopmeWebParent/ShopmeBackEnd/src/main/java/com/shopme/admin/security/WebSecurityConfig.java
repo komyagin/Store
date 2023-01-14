548b6b0a414446c2e15d0215e5a33ed095734852
komyagin <Semyon_Komyagin@epam.com>
2023-01-15 01:01:03 +0500

    Initial commit

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.shopme.admin.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new ShopmeUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/images/**", "/js/**", "/webjars/**")
+                .permitAll().and().authorizeHttpRequests().requestMatchers("/**")
+                .authenticated().and()
+                .userDetailsService(userDetailsService())
+                .authenticationProvider(authenticationProvider())
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .permitAll()
+                .and()
+                .logout().permitAll();
+        return http.build();
+    }
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserCsvExporter.java
@@ -0,0 +1,26 @@
+package com.shopme.admin.user;
+
+import com.shopme.common.entity.User;
+import jakarta.servlet.http.HttpServletResponse;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import java.io.IOException;
+import java.util.List;
+
+public class UserCsvExporter extends AbstractExporter {
+    public void export(List<User> userList, HttpServletResponse response) throws IOException {
+        setResponseHeader(response, "text/csv", "csv");
+
+        ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+
+        String[] csvHeader = {"User ID", "Email", "First Name", "Last Name", "Roles", "Enabled"};
+        String[] fieldMapping = {"id", "email", "firstName", "lastName", "roles", "enabled"};
+        csvWriter.writeHeader(csvHeader);
+        for (User user : userList) {
+            csvWriter.write(user, fieldMapping);
+        }
+        csvWriter.close();
+    }
+}
