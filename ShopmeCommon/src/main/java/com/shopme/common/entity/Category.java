4a626adb38b411bba81ee5f304eca41c7acdd075
komyagin <Semyon_Komyagin@epam.com>
2023-01-16 21:34:56 +0500

    Section 10 add Category Entity

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -0,0 +1,28 @@
+package com.shopme.common.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 128, nullable = false, unique = true)
+    private String name;
+    @Column(length = 64, nullable = false, unique = true)
+    private String alias;
+    @Column(length = 128, nullable = false)
+    private String image;
+
+    private boolean enabled;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> children = new HashSet<>();
+}
